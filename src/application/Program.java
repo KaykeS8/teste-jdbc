40f3be02430fd84fca53f393ef042bec0cef774a
Kayke simao <kayke.py@gmail.com>
2026-03-16 20:41:37 -0300

    Updating with JDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,45 +1,29 @@
 package application;
 
 import db.DB;
-import db.DbException;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Program {
     public static void main(String[] args) {
         Connection conn = null;
-        PreparedStatement pt = null;
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        PreparedStatement st = null;
 
         try {
             conn = DB.getConnection();
-            pt = conn.prepareStatement(
-                    "INSERT INTO seller " +
-                       "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
-                       "VALUES " +
-                       "(?, ?, ?, ?, ?)", PreparedStatement.RETURN_GENERATED_KEYS
+            st = conn.prepareStatement(
+                    "UPDATE seller SET BaseSalary = BaseSalary * ? + BaseSalary  WHERE id = ?"
             );
-            pt.setString(1, "Kayke simao");
-            pt.setString(2, "Kayke.simao@gmail.com");
-            pt.setDate(3, new Date(sdf.parse("21/08/2004").getTime()));
-            pt.setDouble(4, 4800);
-            pt.setInt(5, 5);
 
-            int rowAffecteds = pt.executeUpdate();
-            if (rowAffecteds > 0) {
-                ResultSet rs  = pt.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("DONE insertion ID=" + id);
-                }
-            } else {
-                System.out.println("No row affected");
-            }
-        } catch (SQLException | ParseException e) {
-            throw new DbException(e.getMessage());
+            st.setDouble(1, 0.3);
+            st.setInt(2, 7);
+
+            int rowsAffected = st.executeUpdate();
+            System.out.println("Done! rows affected: " + rowsAffected);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
