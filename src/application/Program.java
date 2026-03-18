f9fec79e2a6580b428e9c8f547c3e6c42341bb38
Kayke simao <kayke.py@gmail.com>
2026-03-18 20:59:33 -0300

    Implementing the DAO design pattern

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,48 +1,19 @@
 package application;
 
-import db.DB;
-import db.DbException;
-import db.DbIntegrityException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement st = null;
+        Department dept = new Department(1 ,"TI");
+        Seller seller1 = new Seller(1, "kayke", "kayke.simao@gmail.com", new Date(), 3500.00, dept);
 
-        try {
-            conn = DB.getConnection();
-            st = conn.createStatement();
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-            conn.setAutoCommit(false);
-
-            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
-//            int x = 1;
-//            if( 1 < 2) {
-//                throw new SQLException("fake error");
-//            }
-            int row2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
-
-            conn.commit();
-
-
-            System.out.println("first operation! rows affected: " + rows1);
-            System.out.println("second operation! rows affected: " + row2);
-
-        } catch (SQLException e) {
-            try {
-                conn.rollback();
-                throw new DbException("Transaction rolled back caused by: " + e.getMessage());
-            } catch (SQLException e1) {
-                throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
-            }
-        } finally {
-            DB.closeConnection();
-            DB.closeStatemnt(st);
-        }
+        System.out.println(seller1);
     }
 }
